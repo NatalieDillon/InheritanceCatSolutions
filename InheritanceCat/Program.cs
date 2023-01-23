64e7f57bfc2c1b81ded4bda22f3635575e265f70
NatalieDillon <112569645+NatalieDillon@users.noreply.github.com>
2023-01-23 10:18:34 +0000

    Add link to classes

--- InheritanceCat/Program.cs
+++ InheritanceCat/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using InheritanceCat.Classes;
+
+namespace InheritanceCat
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            DomesticCat cat = new DomesticCat();
+            Console.WriteLine(cat.Eat());
+        }
+    }
+}
